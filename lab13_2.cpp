265a502ca349107fa06f593b508824a9cdab2595
Thanik30 <jeng.thanik@gmail.com>
2024-12-31 18:31:57 +0700

    lab13_2.cpp completed

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,24 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+void updateImage(bool image[][M],int s,int x,int y){
+    for(int i = 0;i<N;i++) {
+        for(int j = 0;j<M;j++){
+            if (pow(i-x,2)+pow(j-y,2)<=pow(s-1,2)) image[i][j] = 1;
+        }
+    }
+}
+void showImage(const bool image[][M]){
+    for (int x1 = 0 ;x1<M+2;x1++) cout<<"-";
+    cout<<"\n";
+    for(int i = 0;i<N;i++) {
+        cout<<"|";
+        for (int j = 0 ;j<M;j++){
+            if (image[i][j]==0) cout<<" ";
+            else if (image[i][j]==1) cout<<"*";
+        }
+        cout<<"|\n";
+    }
+    for (int x1 = 0 ;x1<M+2;x1++) cout<<"-";
+    cout<<"\n";
+}
