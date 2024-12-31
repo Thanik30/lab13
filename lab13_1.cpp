f12b89fd433fc9af391919268eb7c64050b02492
Thanik30 <jeng.thanik@gmail.com>
2024-12-31 18:28:52 +0700

    lab13_1.cpp completed

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -19,3 +19,21 @@ int main(){
     cout << "Min = " << B[5];
     return 0;
 }
+void stat(const double A[],int N,double B[]){
+    double sum,SD,GM = 1,H,max = A[0] ,min  = A[0];
+    for (int i = 0;i<N;i++) sum += A[i];   
+    B[0] = sum / N;
+    double mean = B[0];
+    for (int i = 0;i<N;i++) SD += pow(A[i]-mean,2);
+    B[1] = pow(SD/N,0.5);
+    for (int i = 0;i<N;i++) GM *= A[i];
+    B[2] = pow(GM,1.00/N);
+    for (int i = 0;i<N;i++) H += 1/A[i];
+    B[3] = N/H;
+    for (int i = 1;i<N;i++) {
+        if (A[i] > max) max = A[i];
+        if (A[i] < min) min = A[i];
+    }
+    B[4] = max;
+    B[5] = min;
+}
